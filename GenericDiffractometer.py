cf15440d989f584becc10f2e47269abd08a0f7bd
IvarsKarpics <ivars.karpics@gmail.com>
2016-04-11 17:30:03 +0200

    2016.04.11. Fixed to be compatible with python3

--- GenericDiffractometer.py
+++ GenericDiffractometer.py
@@ -402,7 +402,7 @@ class GenericDiffractometer(HardwareObject):
 
         try:
             centring_method = self.centring_methods[method]
-        except KeyError, diag:
+        except KeyError as diag:
             logging.getLogger("HWR").error("Diffractometer: unknown centring method (%s)" % str(diag))
             self.emit_centring_failed()
         else:
@@ -424,7 +424,7 @@ class GenericDiffractometer(HardwareObject):
             try:
                 #TODO... do we need this at all?
                 fun = self.cancel_centring_methods[self.current_centring_method]
-            except KeyError, diag:
+            except KeyError as diag:
                 self.emit_centring_failed()
             else:
                 try:
